fbb557d515e352d4e1354e9c4831e8483e436fad
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-16 19:54:58 +0900

    Create "medium Permutation in String.py

---
+++ "medium Permutation in String.py
@@ -0,0 +1,16 @@
+def checkInclusion(self, s1: str, s2: str) -> bool:
+    find, have, same = Counter(s1), Counter(s2[:len(s1)]), sum([int(find[c] == have[c]) for c in find])
+
+    for i in range(len(s2) - len(s1)):
+        if same == len(find):
+            return True
+
+        same -= int(find[s2[i]] == have[s2[i]])
+        have[s2[i]] -= 1
+
+        same += int(find[s2[i]] and find[s2[i]] == have[s2[i]]) - int(find[s2[i + len(s1)]] and find[s2[i + len(s1)]] == have[s2[i + len(s1)]])
+
+        have[s2[i + len(s1)]] += 1
+        same += int(find[s2[i + len(s1)]] == have[s2[i + len(s1)]])
+
+    return same == len(find)
