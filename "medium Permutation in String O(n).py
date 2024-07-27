7b1082af6ae2de8cf220bffc8a9956d9c5dd289d
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-27 17:40:45 +0900

    Update "medium Permutation in String O(n).py

--- "medium Permutation in String O(n).py
+++ "medium Permutation in String O(n).py
@@ -2,18 +2,18 @@ def checkInclusion(self, s1: str, s2: str) -> bool:
     same, find, have = 0, Counter(s1), Counter(s2[:len(s1)])
 
     for c in find:
-        same += int(find[c] == have[c])
+        same += find[c] == have[c]
 
     for i in range(len(s2) - len(s1)):
         if same == len(find):
             return True
 
-        same -= int(find[s2[i]] == have[s2[i]])
+        same -= find[s2[i]] == have[s2[i]]
         have[s2[i]] -= 1
 
-        same += int(find[s2[i]] and find[s2[i]] == have[s2[i]]) - int(find[s2[i + len(s1)]] and find[s2[i + len(s1)]] == have[s2[i + len(s1)]])
+        same += (find[s2[i]] and find[s2[i]] == have[s2[i]]) - (find[s2[i + len(s1)]] and find[s2[i + len(s1)]] == have[s2[i + len(s1)]])
 
         have[s2[i + len(s1)]] += 1
-        same += int(find[s2[i + len(s1)]] == have[s2[i + len(s1)]])
+        same += find[s2[i + len(s1)]] == have[s2[i + len(s1)]]
 
     return same == len(find)
